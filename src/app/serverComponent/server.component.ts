e5095d09a4d2a2041b8b2d5197e02b4351fec3ad
Ramakrishna Punjal <rkpunjal@Ramakrishnas-MacBook-Pro.local>
2018-01-29 13:29:36 +0100

    added ngStyle& ngClass

--- src/app/serverComponent/server.component.ts
+++ src/app/serverComponent/server.component.ts
@@ -3,12 +3,31 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
   serverId = 10;   // could do : serverId: number = 10;
-  serverStatus: string = 'offline';  // also type definition is done automatically
+  serverStatus: string = 'Offline';  // also type definition is done automatically
+
+  constructor(){
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
 
   getServerStatus(){
     return this.serverStatus;
   }
+
+
+  getColor(){
+    return this.serverStatus === 'Online' ? 'green' : 'red';
+  }
+
+  isOnline(){
+    return this.serverStatus === 'Online'
+  }
+
 }
